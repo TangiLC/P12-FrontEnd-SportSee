27f21c0ef251c9972cfb23f75721f58ee0c524ec
TangiLC <tangi@le-cadre.net>
2023-08-24 23:41:21 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import LoginPage from './page/LoginPage';
+import DashboardPage from './page/DashboardPage';
+import Page404 from './page/Page404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path={['/home', '/', '/dashboard']}>
+          <LoginPage />
+        </Route>
+        <Route path="/dashboard/:id">
+          <DashboardPage />
+        </Route>
+        <Route path="/dashboard">
+          <Redirect to="/dashboard" />
+        </Route>
+        <Route path="*">
+          <Page404 />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
