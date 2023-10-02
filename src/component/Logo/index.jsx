f0bd7d52b62e06acea6105957bc78a97239d6596
TangiLC <tangi@le-cadre.net>
2023-10-02 23:38:06 +0200

    sass

--- src/component/Logo/index.jsx
+++ src/component/Logo/index.jsx
@@ -4,8 +4,8 @@ import sportSee from "../../assets/sportsee.png";
 
 export default function Logo() {
 	return (
-		<div className="container sportsee-login">
-			<img src={sportSee} alt="logo" className=" logo" />
+		<div className="container sportsee">
+			<img src={sportSee} alt="logo" className="logo" />
 			<h2>SportSee</h2>
 		</div>
 	);
